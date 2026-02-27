394446397e12c52330475dca4d0a164603a7f940
mafeHumnz <humanez036@gmail.com>
2026-02-27 13:48:27 -0500

    Register y login

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -16,3 +16,34 @@ export const registerUser = async (req, res) => {
         res.status(500).json({mensaje: "Error al registrar usuario" })
     }
 };
+
+export const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password){
+            return res.status(400).json("Email y password son obligatorios!")
+        }
+
+        const user = await User.findOne({email});
+
+        if (!user){
+            return res.status(400).json({mensaje: "Usuario no encontrado"});
+        }
+
+        if (password !== user.password){
+           return res.status(400).json("Las contrase;a no coinciden!")
+        }
+
+        res.status(200).json({mensaje: "Login exitoso",
+            user: {
+                id: user._id,
+                nombre: user.nombre,
+                email: user.email,
+                apodo: user.apodo
+            },
+        });
+    } catch (error) {
+        res.status(500).json({mensaje: "Error al hacer login"});
+    }
+};
