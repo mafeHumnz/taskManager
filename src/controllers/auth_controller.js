bac802d39cd2bb283e8079534a169838f70e9ca3
mafeHumnz <humanez036@gmail.com>
2026-03-04 19:50:07 -0500

    Actualizacion: Actenticacion JWT

--- src/controllers/auth_controller.js
+++ src/controllers/auth_controller.js
@@ -1,14 +1,33 @@
+import bcrypt from bcrypt;
 import { User } from "../models/user.js";
 
 export const registerUser = async (req, res) => {
     try {
         const {nombre, email, apodo, password} = req.body;
 
+        if (!nombre || !email || !password) {
+      return res.status(400).json({
+        mensaje: "Campos obligatorios faltantes",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        mensaje: "El email ya está registrado",
+      });
+    }
+
+    // 🔥 Hashear password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
         const newUser = await User.create({
             nombre,
             email,
             apodo,
-            password
+            password: hashedPassword
         });
 
         res.status(201).json(newUser);
@@ -31,9 +50,21 @@ export const loginUser = async (req, res) => {
             return res.status(400).json({mensaje: "Usuario no encontrado"});
         }
 
-        if (password !== user.password){
-           return res.status(400).json("Las contrase;a no coinciden!")
-        }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        mensaje: "Credenciales incorrectas",
+      });
+    }
+
+    // 🔥 Aquí generamos el token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+
 
         res.status(200).json({mensaje: "Login exitoso",
             user: {
@@ -41,7 +72,7 @@ export const loginUser = async (req, res) => {
                 nombre: user.nombre,
                 email: user.email,
                 apodo: user.apodo
-            },
+            }, token,
         });
     } catch (error) {
         res.status(500).json({mensaje: "Error al hacer login"});
